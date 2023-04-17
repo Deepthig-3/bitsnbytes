9b262ca3f2381d285da4a24f1a911813e6eb016a
Deepthig-3 <131008900+Deepthig-3@users.noreply.github.com>
2023-04-17 20:40:14 +0530

    Add files via upload

---
+++ Bitsndbytes/bnb_home_html.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="C:/deepthi/Bitsndbytes/bnb_css.css" />
+</head>
+
+<body>
+    <section class="header">
+        <a href="C:/deepthi/Bitsndbytes/bnb_home_html.php" class="logo">BITSNBYTES</a>
+        <nav class="navbar">
+            <a href="C:/deepthi/Bitsndbytes/bnb_home_html.php">Home</a>
+            <a href="C:/deepthi/Bitsndbytes/bnb_about_html.php">About</a>
+            <a href="C:/deepthi/Bitsndbytes/bnb_events_html.php">Events</a>
+            <a href="C:/deepthi/Bitsndbytes/bnb_login_html.php">Login</a>
+
+        </nav>
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script src="C:/deepthi/Bitsndbytes/bnb_js.js"></script>
+</body>
+
+</html>
